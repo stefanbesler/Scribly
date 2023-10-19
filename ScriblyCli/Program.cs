59987fe3be5f64cfbd3141bf694b13eed31b949f
Stefan Besler <stefan@besler.me>
2023-10-19 22:38:35 +0200

    feat: initial commit

---
+++ ScriblyCli/Program.cs
@@ -0,0 +1,19 @@
+﻿static void ProcessUserInput(char input)
+{
+
+}
+
+
+Console.WriteLine("Type something (press Esc to exist):");
+
+while (true)
+{
+    var keyInfo = Console.ReadKey(intercept: true);
+
+    if (keyInfo.Key == ConsoleKey.Escape)
+        break;
+
+    ProcessUserInput(keyInfo.KeyChar);
+}
+
+Console.WriteLine("Fin.");
